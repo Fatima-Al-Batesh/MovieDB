3dccf6baabe082f7282f242356ebe5e74d16e2a5
Fatima Al-Batesh <fbatesh@gmail.com>
2020-10-21 19:56:07 +0300

    step 5

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const movies = [
+  { title: 'Jaws', year: 1975, rating: 8 },
+  { title: 'Avatar', year: 2009, rating: 7.8 },
+  { title: 'Brazil', year: 1985, rating: 8 },
+  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
 app.get('/', (req, res) => {
   res.send('ok')
 })
@@ -25,7 +32,7 @@ app.get('/hello/:ID', (req, res) => {
     res.send(
       {status:200, message:"Hello, "+ data.ID}
     )
-  })
+})
 app.get('/search', (req, res) => {
   search=req.query.s
     if (!search){
@@ -38,7 +45,43 @@ app.get('/search', (req, res) => {
       )
     }
 })
+//create
+app.get('/movies/create', (req, res) => {
+  res.send(
+      {
+           status:200,
+           data:"ok"
+      }
+  )
+})
 
+//read
+app.get('/movies/read', (req, res) => {
+  res.send(
+      {
+           status:200,
+           data:movies
+      }
+  )
+})
+//update
+app.get('/movies/update', (req, res) => {
+  res.send(
+      {
+           status:200,
+           data:"ok"
+      }
+  )
+})
+//delete
+app.get('/movies/delete', (req, res) => {
+  res.send(
+      {
+           status:200,
+           data:"ok"
+      }
+  )
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
